c8ef9c165e0bd600761becd60150d9e9078c2bab
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-05-09 02:47:18 +0600

    Update New Traffic System.cpp

--- LightOJ/New Traffic System.cpp
+++ LightOJ/New Traffic System.cpp
@@ -7,6 +7,7 @@ typedef long long ll;
 #define endl "\n" 
 #define all(v) v.begin(), v.end()
 #define fr(x, n) for (int i = x; i < n; ++i)
+#define tupll tuple<ll, ll, ll>
 const int N = 1e4+12;
 const ll mx = 1e17;
 
@@ -18,29 +19,30 @@ struct hella
 vector<hella> g[N];
 ll dist[11][N];
 
-void dijkstra(ll src, ll n, ll d) {
-    for(int i=0; i<11; i++) {
-        for(int j=0; j<N; j++) dist[i][j] = mx;
+void dijkstra(ll src, ll n, ll d)
+{
+    for(int i=0; i<=d; i++) {
+        for(int j=0; j<=n; j++) dist[i][j] = mx;
     }
     dist[0][src] = 0;
 
-    priority_queue< vll, vector<vll>, greater<vll> >pq;
-    pq.push({0, 0, src});
+    priority_queue< tupll, vector<tupll>, greater<tupll>> pq;
+    pq.push({0 , 0, src});
 
-    while(!pq.empty()) {
-        ll curD = pq.top()[0], ct = pq.top()[1], u = pq.top()[2];
+    while(!pq.empty())
+    {
+        ll curD = get<0>(pq.top()), ct = get<1>(pq.top()), u = get<2>(pq.top());
         pq.pop();
         if(curD > dist[ct][u]) continue;
-
-        for(auto edge : g[u]) {
-           ll v = edge.v, weight = edge.w, typ = edge.typ;
-           if(typ + ct <= d && dist[typ + ct][v] > dist[ct][u] + weight) {
+        for(auto edge : g[u])
+        {
+            ll v = edge.v, weight = edge.w, typ = edge.typ;
+            if(typ + ct <= d && dist[typ + ct][v] > dist[ct][u] + weight) {
                 dist[typ + ct][v] = dist[ct][u] + weight;
                 pq.push({dist[typ + ct][v], typ+ct, v});
-           }            
+            }
         }
     }
-
 }
 
 void solve() {
@@ -54,14 +56,12 @@ void solve() {
         ll u, v, w; cin >> u >> v >> w;
         g[u].push_back({v, w, 1});
     }
-
     dijkstra(0, n, d);
     ll mini = mx;
-    for(int i=0; i<=d; i++)  {
+    for(int i=0; i<=d; i++) {
         mini = min(mini, dist[i][n-1]);
     }
-    if(mini == mx) cout << "Impossible" << endl;
-    else cout << mini << endl; 
+    mini == mx ? cout << "Impossible\n" : cout << mini << endl;
 }
 
 // --- Try to formulate equations. ---
