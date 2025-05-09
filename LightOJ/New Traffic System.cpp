b5468b6b8d82b0bf3957c7e50abccf2f7ec9e274
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-05-10 01:57:21 +0600

    Update New Traffic System.cpp

--- LightOJ/New Traffic System.cpp
+++ LightOJ/New Traffic System.cpp
@@ -7,47 +7,42 @@ typedef long long ll;
 #define endl "\n" 
 #define all(v) v.begin(), v.end()
 #define fr(x, n) for (int i = x; i < n; ++i)
-#define tupll tuple<ll, ll, ll>
 const int N = 1e4+12;
 const ll mx = 1e17;
 
-struct hella
-{
-    ll v, w, typ;
-};
-
-vector<hella> g[N];
+#define tupll tuple<ll, ll, ll>
+vector<tupll>g[N];
 ll dist[11][N];
 
 void dijkstra(ll src, ll n, ll d)
 {
     for(int i=0; i<=d; i++) {
-        for(int j=0; j<=n; j++) dist[i][j] = mx;
+        for(int j=0; j<n; j++) dist[i][j] = mx;
     }
     dist[0][src] = 0;
 
-    priority_queue< tupll, vector<tupll>, greater<tupll>> pq;
-    pq.push({0 , 0, src});
-
-    while(!pq.empty())
-    {
-        ll curD = get<0>(pq.top()), ct = get<1>(pq.top()), u = get<2>(pq.top());
+    priority_queue<tupll, vector<tupll>, greater<tupll>> pq;
+    pq.push({0, 0, src});
+    
+    while(!pq.empty()) {
+        auto [curD, ct, u] = pq.top();
         pq.pop();
         if(curD > dist[ct][u]) continue;
-        for(auto edge : g[u])
-        {
-            ll v = edge.v, weight = edge.w, typ = edge.typ;
-            if(typ + ct <= d && dist[typ + ct][v] > dist[ct][u] + weight) {
-                dist[typ + ct][v] = dist[ct][u] + weight;
-                pq.push({dist[typ + ct][v], typ+ct, v});
-            }
+
+        for(auto [v, weight, typ] : g[u]) {
+            if(typ + ct <= d && dist[typ+ct][v] > dist[ct][u] + weight) {
+                dist[typ+ct][v] = dist[ct][u] + weight;
+                pq.push({dist[typ+ct][v], typ+ct, v});
+            } 
         }
     }
 }
 
+
 void solve() {
     ll n, m, k, d; cin >> n >> m >> k >> d;
-    for(int i=0; i<=n; i++) g[i].clear();
+    for(int i=0; i<n; i++) g[i].clear();
+   
     for(int i=0; i<m; i++) {
         ll u, v, w; cin >> u >> v >> w;
         g[u].push_back({v, w, 0});
@@ -56,11 +51,13 @@ void solve() {
         ll u, v, w; cin >> u >> v >> w;
         g[u].push_back({v, w, 1});
     }
+
     dijkstra(0, n, d);
-    ll mini = mx;
+
+    ll mini =  mx;
     for(int i=0; i<=d; i++) {
         mini = min(mini, dist[i][n-1]);
-    }
+    }    
     mini == mx ? cout << "Impossible\n" : cout << mini << endl;
 }
 
